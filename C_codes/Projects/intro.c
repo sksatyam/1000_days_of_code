0d4114c8d8aa15f2c7e8eb9a9a609d8cf857996b
sksatyam <killersksatyam@gmail.com>
2023-11-27 00:53:11 +0530

    Added intro.c in C_codes/Projects Repo

---
+++ C_codes/Projects/intro.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    // Here in this Repo I will be uploading all the projects that I will create using C programing language
+    printf("Hello 1000_days_of_code");
+    
+    return 0;
+}
