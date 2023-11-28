0628a44248f3e97685a1854b7fc019d4bcccbe55
sksatyam <killersksatyam@gmail.com>
2023-11-29 00:33:02 +0530

    This programs first takes arrar as an input from user and then it gives the largest element but this program doesnt works with array of all negative elemnt.

---
+++ C_codes/Practise_Questions/29_largest_element.c
@@ -0,0 +1,21 @@
+// 29. Write a program to find the largest element in an array.
+#include<stdio.h>
+int main(){
+    int arlen,largest_element = 0;
+    printf("Enter the length of the array that you want enter: ");
+    scanf("%d",&arlen);
+    int arr[arlen];
+    printf("Enter elements of array seperate by whitespace: ");
+    for (int i=0;i<arlen;i++){
+        scanf("%d",&arr[i]);
+    }
+    for (int j=0;j<arlen;j++){
+        if (arr[j]>largest_element){
+            largest_element = arr[j];
+            }
+    }
+    printf("The largest element in the array is: %d",largest_element);
+    
+    return 0;
+
+}
