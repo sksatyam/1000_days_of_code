a2414ae6cb08b1221a08fb549525a1cc151b4319
sksatyam <killersksatyam@gmail.com>
2023-11-30 23:51:19 +0530

    Just Wrote minimum code for Finding LCM of two number

---
+++ C_codes/Practise_Questions/36_LCM_of_two_number.c
@@ -0,0 +1,9 @@
+//write a program to find LCM of two number
+#include<stdio.h>
+int main(){
+    int firstNum,secondNum;
+    printf("Enter two numbers and Seperate them by whitespace: ");
+    scanf("%d %d",&firstNum,&secondNum);
+    printf("%d %d",firstNum,secondNum);
+    return 0;
+}
