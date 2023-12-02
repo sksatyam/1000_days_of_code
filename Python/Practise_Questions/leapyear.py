89677600db793bed94455779f0f293d6d2d207ff
sksatyam <killersksatyam@gmail.com>
2023-12-02 22:42:06 +0530

    This program takes a year as an input from user and tells weather that year is leap year or not

--- Python/Practise_Questions/leapyear.py
+++ Python/Practise_Questions/leapyear.py
@@ -1,1 +1,12 @@
 # Write a program to take a year as an input from the user and then display whether the following year is a leap year or not.
+userInput = int(input("Enter a year: "))
+if userInput % 4 == 0:
+    if userInput % 100 == 0:
+        if userInput % 400 == 0:
+            print("This year is a leap year")
+        else:
+            print("This is not a leap year")
+    else:
+        print("This year is a leap year.u")
+else:
+    print("The year is not a leap year.")
