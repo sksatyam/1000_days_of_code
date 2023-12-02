be192acc9ca1f8f55a491c40c3cafbc8beb1688a
sksatyam <killersksatyam@gmail.com>
2023-12-02 22:45:00 +0530

    Just a litter change in output prompt at line no.6,8,10,12

--- Python/Practise_Questions/leapyear.py
+++ Python/Practise_Questions/leapyear.py
@@ -3,10 +3,10 @@ userInput = int(input("Enter a year: "))
 if userInput % 4 == 0:
     if userInput % 100 == 0:
         if userInput % 400 == 0:
-            print("This year is a leap year")
+            print("This year is a leap year.")
         else:
-            print("This is not a leap year")
+            print("This is not a leap year.")
     else:
-        print("This year is a leap year.u")
+        print("This year is a leap year.")
 else:
     print("The year is not a leap year.")
