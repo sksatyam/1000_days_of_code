13343b05f13b90a0eff0af567990d29e8d3002e1
sksatyam <killersksatyam@gmail.com>
2023-12-03 21:34:15 +0530

    Write a working code

--- C_codes/Practise_Questions/37_pyramid_Pattern.c
+++ C_codes/Practise_Questions/37_pyramid_Pattern.c
@@ -1,5 +1,19 @@
 // Write a program to print pyramid Pattern
-#include<stdio.h>
-int main(){
-    
+#include <stdio.h> 
+int main() 
+{ 
+	int rows;
+    printf("Enter the number of rows: ");
+    scanf("%d",&rows);  
+	for (int i = 0; i < rows; i++) { 
+		for (int j = 0; j < 2 * (rows - i) - 1; j++) { 
+			printf(" "); 
+		}  
+		for (int k = 0; k < 2 * i + 1; k++) { 
+			printf("* "); 
+		} 
+		printf("\n"); 
+	} 
+	return 0; 
 }
+
