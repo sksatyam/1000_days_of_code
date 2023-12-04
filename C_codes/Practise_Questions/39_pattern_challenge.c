10a62cb53fe7d2902f40514afe16f8367c857d6d
sksatyam <killersksatyam@gmail.com>
2023-12-04 23:19:52 +0530

    Challenge Completed, one of the hardest question I still dont know how its working, may be i will come in future to properly understand this program

--- C_codes/Practise_Questions/39_pattern_challenge.c
+++ C_codes/Practise_Questions/39_pattern_challenge.c
@@ -8,7 +8,22 @@ Write a program to print the following pattern:
 */
 #include<stdio.h>
 int main(){
-    int row = 5;
-    
+    int row = 5,cpRow;
+    cpRow =row;
+    for (int i=1;i<=row;i++){
+        for (int j=1;j<=i;j++){
+            if (j == 1)
+            {
+                for (int k=1;k<=(row-i);k++){
+                    printf("  ");
+                } 
+            }
+            printf("%d ",j);
+        }
+        for (int l=0;l<(i-1);l++){
+            printf("%d ",(i-1)-l);
+            }
+        printf("\n");
+    }
     return 0;
 }
