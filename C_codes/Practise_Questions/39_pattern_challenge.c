11aecb3ac5a483a587ecebfecbf0f42005e86511
sksatyam <killersksatyam@gmail.com>
2023-12-04 22:10:50 +0530

    New pattern, challenge accepted

---
+++ C_codes/Practise_Questions/39_pattern_challenge.c
@@ -0,0 +1,14 @@
+/*
+Write a program to print the following pattern:
+        1
+      1 2 1
+    1 2 3 2 1
+  1 2 3 4 3 2 1
+1 2 3 4 5 4 3 2 1
+*/
+#include<stdio.h>
+int main(){
+    int row = 5;
+    
+    return 0;
+}
