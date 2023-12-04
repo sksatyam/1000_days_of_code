3c0da4feec2acce8a177283be249bb116c5e5111
sksatyam <killersksatyam@gmail.com>
2023-12-04 21:58:37 +0530

    .\38_num_pattern.c Added, warmup code

---
+++ C_codes/Practise_Questions/38_num_pattern.c
@@ -0,0 +1,20 @@
+/*
+Write a program to print the following:
+0
+1 2
+3 4 5
+6 7 8 9
+*/
+#include<stdio.h>
+int main(){
+    int row = 4,num = 0;
+    for (int i=1;i<=row;i++){
+        for (int j=0;j<i;j++){
+            printf("%d ",num);
+            num ++;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
