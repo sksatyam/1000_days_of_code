f159a853ddda0beb9e30e78031f9c016d3285a5c
sksatyam <killersksatyam@gmail.com>
2023-12-06 00:52:57 +0530

    New Program started, level tough one

---
+++ C_codes/Practise_Questions/40_spiral_matrix.c
@@ -0,0 +1,6 @@
+// Write a program in which user will first enter a matrix and than our program shuld return the values of the matrix at the spiral positions od the matrix
+#include<stdio.h> // we are going to use scanf and printf so, I am  including it.
+int main(){
+    // My Sudo code behind this program is that forst I am going ot make a functin in which I will pass pointer of some variable or some array this functin mainly take input from user and store it into two 2d_arrays
+    return 0;
+}
