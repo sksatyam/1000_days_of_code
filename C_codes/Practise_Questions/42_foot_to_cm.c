6675a3b4574e62c9101f51d877575e4c1f9e5454
sksatyam <killersksatyam@gmail.com>
2023-12-09 23:21:35 +0530

    .\42_foot_to_cm.c Added

--- C_codes/Practise_Questions/42_foot_to_cm.c
+++ C_codes/Practise_Questions/42_foot_to_cm.c
@@ -4,6 +4,6 @@ int main(){
     int foot;
     printf("Enter a number you want to convert: ");
     scanf("%d",&foot);
-    printf("%d foots is equals to %f cm",foot,foot*30.48);
+    printf("%d foots is equals to %fcm",foot,foot*30.48);
     return 0;
 }
