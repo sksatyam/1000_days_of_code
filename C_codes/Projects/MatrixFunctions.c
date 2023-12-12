dcfaf90035b59cb3fcb3eaaa2ff337bb53d74612
sksatyam <killersksatyam@gmail.com>
2023-12-12 22:13:26 +0530

    Just Making a Hobby project which will be able to perform some operations on matrix or 2d array

---
+++ C_codes/Projects/MatrixFunctions.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void arrayRow(int arr[][k], int a, int c){
+    for (int i=0;i<4;i++){
+            printf(" %d", arr[a][i]);
+        }
+    
+    }
+int main(){
+    int array1[][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+    int l = 4;
+    arrayRow(array1[][l],3,4);
+    return 0;
+}
