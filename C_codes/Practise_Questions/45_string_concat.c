ebd99cb11e99c7647c0552938ee130a6701f8795
sksatyam <killersksatyam@gmail.com>
2023-12-14 19:16:35 +0530

    this program I done but didnt understatnd how its working

--- C_codes/Practise_Questions/45_string_concat.c
+++ C_codes/Practise_Questions/45_string_concat.c
@@ -1,1 +1,20 @@
 // Write a program to concatinate two strings
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    void conc(char*,char*);
+    char st1[25]= "Kutta ",st2[25] = "Harami";
+    conc(st1,st2);
+    printf("%s",st1);
+    return 0;
+
+}
+void conc(char st1[], char st2[]){
+    int j,i;
+    j = strlen(st1);
+    for (i=0;st2[i] != '\0';i++){
+        st1[j++] = st2[i];
+    }
+    st1[j] = '\0';
+}
