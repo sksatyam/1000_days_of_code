3ba15ef85beff1db345938b1162d8fc208450f2d
sksatyam <killersksatyam@gmail.com>
2023-12-14 19:28:14 +0530

    this code is incomplete i will get back to this prob again

--- C_codes/Practise_Questions/45_string_concat.c
+++ C_codes/Practise_Questions/45_string_concat.c
@@ -6,7 +6,9 @@ int main(){
     void conc(char*,char*);
     char st1[25]= "Kutta ",st2[25] = "Harami";
     conc(st1,st2);
-    printf("%s",st1);
+    for (int i=0;i<25;i++){
+        printf(" %c", st1[i]);
+    }
     return 0;
 
 }
@@ -14,7 +16,7 @@ void conc(char st1[], char st2[]){
     int j,i;
     j = strlen(st1);
     for (i=0;st2[i] != '\0';i++){
-        st1[j++] = st2[i];
+        st1[(j+i)] = st2[i];
     }
     st1[j] = '\0';
 }
