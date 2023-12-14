73f48e196a8602dd3f60d024b20b0b6a1122ac7a
sksatyam <killersksatyam@gmail.com>
2023-12-14 12:29:51 +0530

    solved Question without using any prewritten function from any libary

--- C_codes/Practise_Questions/44_copy_string.c
+++ C_codes/Practise_Questions/44_copy_string.c
@@ -1,8 +1,18 @@
 // Write a program to copy a string from one variable to another
 
 #include<stdio.h>
+#include<string.h>
+void strcopy(char *arr, char *arr2){
+    int i;
+    for (i=0;arr[i] != '\0' ;i++){
+        arr2[i] = arr [i];
+    }
+    arr2[i] = '\0';
+}
 int main(){
     char str[] = "Sanjib Kumar Sah";
-
+    char str2[50];
+    strcopy(str,str2);
+    printf("%s",str2);
     return 0;
 }
