53ab313be2fa343f89b7533cd609ac425de3fab1
sksatyam <killersksatyam@gmail.com>
2023-12-21 01:41:17 +0530

    created a function that will print the elements on any row in a 2d array

--- C_codes/Practise_Questions/40_spiral_matrix.c
+++ C_codes/Practise_Questions/40_spiral_matrix.c
@@ -1,19 +1,14 @@
 // Write a program in which user will first enter a matrix and than our program shuld return the values of the matrix at the spiral positions od the matrix
 #include<stdio.h> // we are going to use scanf and printf so, I am  including it.
-void inpu(int *row,int *col,int *arr);
-int main(){
-    int rows,col;
-    printf("Enter the order of Matrix m X n : ");
-    scanf("%d %d",&rows,&col);
-    int arr[rows][col];
-    inpu(rows,col,arr);
-    return 0;
-}
-void inpu(int *row,int *col,int *ar){
-    for (int i=0;i<row;i++){
-        for (int j=0;j<col;j++){
-            scanf("%d",&*ar);
-        }        
+void spriralprintrow(int arr[][4], int rowno){//This function is going to print any row you wish
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%d  ",arr[rowno][i]);
     }
-
+    
+}
+int main(){
+    int arr[4][4] = {{1,2,3,11},{4,5,6,12},{7,8,9,13},{15,16,17,18}};
+    spriralprintrow(arr,2);
+    return 0;
 }
