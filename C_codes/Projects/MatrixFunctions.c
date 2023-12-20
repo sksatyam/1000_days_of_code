4ed69d8b9916b24426c71fb76c39a115ad6a4915
sksatyam <killersksatyam@gmail.com>
2023-12-21 00:38:35 +0530

    working on a function to take input in a 2d array

--- C_codes/Projects/MatrixFunctions.c
+++ C_codes/Projects/MatrixFunctions.c
@@ -1,14 +1,22 @@
 // In this file i am going to create some functions that will perform some basic operations on a matrix
 #include<stdio.h>
-void arrayRow(int *arr){
-    for (int i=0;i<4;i++){
-            printf(" %d", arr);
-        }
+void TwodArrayInput(int *arr,int Row, int Col){
+    int temp;
+    for (int i = 0; i < Row*Col; i++)
+    {  
+
+        scanf("%d",&temp);
+        arr +i = temp;
+    }
+    
     
     }
 int main(){
-    int array1[][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-    int l = 4;
-    arrayRow(array1);
+    int Row, Col;
+    printf("Enter the number of Rows and column of the matrix: ");
+    scanf("%d %d",&Row,&Col);
+    int array1[Row][Col];
+    TwodArrayInput(array1,Row,Col);
+    printf("%d %d %d",array1[0][0],array1[1][1],array1[2][2]);
     return 0;
 }
