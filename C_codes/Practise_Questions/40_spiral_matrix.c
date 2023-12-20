7a9890c9a946f376f456cc737c55b9c18dbe0c2a
sksatyam <killersksatyam@gmail.com>
2023-12-21 01:51:19 +0530

    Now I added a feature in function named spiralprintrow that will print the element within a provided range lower limit will be included but uper limit will be excluded

--- C_codes/Practise_Questions/40_spiral_matrix.c
+++ C_codes/Practise_Questions/40_spiral_matrix.c
@@ -1,7 +1,7 @@
 // Write a program in which user will first enter a matrix and than our program shuld return the values of the matrix at the spiral positions od the matrix
 #include<stdio.h> // we are going to use scanf and printf so, I am  including it.
-void spriralprintrow(int arr[][4], int rowno){//This function is going to print any row you wish
-    for (int i = 0; i < 4; i++)
+void spriralprintrow(int arr[][4], int rowno, int lowlim,int uperlim){//This function is going to print any row you wish
+    for (int i = lowlim; i < uperlim ; i++)
     {
         printf("%d  ",arr[rowno][i]);
     }
@@ -16,6 +16,6 @@ void spriralprintcol(int arr[][4], int colno){//This function is going to print 
 }
 int main(){
     int arr[4][4] = {{1,2,3,11},{4,5,6,12},{7,8,9,13},{15,16,17,18}};
-    spriralprintrow(arr,2);
+    spriralprintrow(arr,2,1,3);
     return 0;
 }
