9c828f7507c77feb39c0e8923dc68453924bfe67
sksatyam <killersksatyam@gmail.com>
2023-12-21 01:18:11 +0530

    it still not working not getting desired output

--- C_codes/Projects/MatrixFunctions.c
+++ C_codes/Projects/MatrixFunctions.c
@@ -1,12 +1,13 @@
 // In this file i am going to create some functions that will perform some basic operations on a matrix
 #include<stdio.h>
-void TwodArrayInput(int arr[][100],int Row, int Col){
+void twodArrayInput(int *arr[][100],int Row, int Col){
     int temp;
     for (int i = 0; i < Row; i++)
     {
         for (int  j = 0; j < Col; j++)
         {
-            scanf("%d",&arr[i][j]);
+            scanf("%d",&temp);
+            arr[i][j] = temp;
         }
         
     }
@@ -14,11 +15,11 @@ void TwodArrayInput(int arr[][100],int Row, int Col){
     
     }
 int main(){
-    int Row, Col;
-    printf("Enter the number of Rows and column of the matrix: ");
-    scanf("%d %d",&Row,&Col);
+    int Row=3, Col =3;
+    // printf("Enter the number of Rows and column of the matrix: ");
+    // scanf("%d %d",&Row,&Col);
     int array1[100][100];
-    TwodArrayInput(array1,Row,Col);
+    twodArrayInput(array1,Row,Col);
     printf("%d %d %d",array1[0][0],array1[1][1],array1[2][2]);
     return 0;
 }
