2a4fe8cdcb7d6476134567738b6b2276bbd22e48
sksatyam <killersksatyam@gmail.com>
2023-12-21 11:06:40 +0530

    decleared basic variables and thinking about logic

--- C_codes/Practise_Questions/40_spiral_matrix.c
+++ C_codes/Practise_Questions/40_spiral_matrix.c
@@ -9,7 +9,6 @@ int spriralprintrow(int arr[][4], int rowno, int lowlim,int uperlim){//This func
         count++;
     }
     return count;
-    
 }
 int spriralprintcol(int arr[][4], int colno,int lowlim,int uperlim){//This function is going to print any row you wish
     // This function will print alements of a specific col within a range this include lower limit but excludes upperlimit
@@ -20,14 +19,16 @@ int spriralprintcol(int arr[][4], int colno,int lowlim,int uperlim){//This funct
         count ++;
     }
     return count;
-    
 }
 int main(){
-    int arr[4][4] = {{1,2,3,11},{4,5,6,12},{7,8,9,13},{15,16,17,18}};
-    for (int i = 0; i < 16; i++)
-    {
+    int cnt = 1,rowno = 0,colno = 0,arr[4][4] = {{1,2,3,11},{4,5,6,12},{7,8,9,13},{15,16,17,18}};
+    int lowlimrow = 0, upperlimrow = 5,lowlimcol = 0,upperlimcol = 5,rowcount = 0,colcount = 0;
+    while(cnt <=16){
+        if ((cnt%20 )!=0)
+        {
+            cnt += spriralprintrow(arr);
+        }
         
     }
-    
     return 0;
 }
