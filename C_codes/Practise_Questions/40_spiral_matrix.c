6b25355cfee60f809f6365fea2cef5d4676f7bfe
sksatyam <killersksatyam@gmail.com>
2023-12-21 10:38:50 +0530

    added  new feature in spiralprintrow that will return how many elements are printed or accessed

--- C_codes/Practise_Questions/40_spiral_matrix.c
+++ C_codes/Practise_Questions/40_spiral_matrix.c
@@ -1,11 +1,14 @@
 // Write a program in which user will first enter a matrix and than our program shuld return the values of the matrix at the spiral positions od the matrix
 #include<stdio.h> // we are going to use scanf and printf so, I am  including it.
-void spriralprintrow(int arr[][4], int rowno, int lowlim,int uperlim){//This function is going to print any row you wish
+int spriralprintrow(int arr[][4], int rowno, int lowlim,int uperlim){//This function is going to print any row you wish
     // This function will print alements of a specific row within a range this include lower limit but excludes upperlimit
+    int count =0;
     for (int i = lowlim; i < uperlim ; i++)
     {
         printf("%d  ",arr[rowno][i]);
+        count++;
     }
+    return count;
     
 }
 void spriralprintcol(int arr[][4], int colno,int lowlim,int uperlim){//This function is going to print any row you wish
@@ -18,6 +21,10 @@ void spriralprintcol(int arr[][4], int colno,int lowlim,int uperlim){//This func
 }
 int main(){
     int arr[4][4] = {{1,2,3,11},{4,5,6,12},{7,8,9,13},{15,16,17,18}};
-    spriralprintrow(arr,2,1,3);
+    for (int i = 0; i < 16; i++)
+    {
+        
+    }
+    
     return 0;
 }
