8582a9b5d9b919bc8cf40497adeecfb73e809cd1
sksatyam <killersksatyam@gmail.com>
2023-12-23 16:00:39 +0530

    Formated code to increase readablity

--- C_codes/Practise_Questions/40_spiral_matrix.c
+++ C_codes/Practise_Questions/40_spiral_matrix.c
@@ -1,80 +1,87 @@
 // Write a program in which user will first enter a matrix and than our program shuld return the values of the matrix at the spiral positions od the matrix
-#include<stdio.h> // we are going to use scanf and printf so, I am  including it.
-int spriralprintrow(int arr[][4], int rowno, int lowlim,int uperlim){//This function is going to print any row you wish
+#include <stdio.h> // we are going to use scanf and printf so, I am  including it.
+int spriralprintrow(int arr[][4], int rowno, int lowlim, int uperlim)
+{ // This function is going to print any row you wish
     // This function will print alements of a specific row within a range this include lower limit but excludes upperlimit
-    int count =0;
-    for (int i = lowlim; i < uperlim ; i++)
-    {
-        printf("%d  ",arr[rowno][i]);
-        count++;
-    }
-    return count;
-}
-
-int spriralprintrowrev(int arr[][4], int rowno, int lowlim,int uperlim){//This function is going to print any row you wish
-    // This function will print alements of a specific row within a range this include lower limit but excludes upperlimit
-    int count =0;
-    for (int i = (uperlim-1); i >=lowlim ; i--)
-    {
-        printf("%d  ",arr[rowno][i]);
-        count++;
-    }
-    return count;
-}
-int spriralprintcol(int arr[][4], int colno,int lowlim,int uperlim){//This function is going to print any row you wish
-    // This function will print alements of a specific col within a range this include lower limit but excludes upperlimit
-    int count =0;
+    int count = 0;
     for (int i = lowlim; i < uperlim; i++)
     {
-        printf("%d  ",arr[i][colno]);
-        count ++;
+        printf("%d  ", arr[rowno][i]);
+        count++;
     }
     return count;
 }
 
-int spriralprintcolrev(int arr[][4], int colno,int lowlim,int uperlim){//This function is going to print any row you wish
-    // This function will print alements of a specific col within a range this include lower limit but excludes upperlimit
-    int count =0;
-    for (int i = (uperlim-1); i >= lowlim; i--)
+int spriralprintrowrev(int arr[][4], int rowno, int lowlim, int uperlim)
+{ // This function is going to print any row you wish
+    // This function will print alements of a specific row within a range this include lower limit but excludes upperlimit
+    int count = 0;
+    for (int i = (uperlim - 1); i >= lowlim; i--)
     {
-        printf("%d  ",arr[i][colno]);
-        count ++;
+        printf("%d  ", arr[rowno][i]);
+        count++;
     }
     return count;
 }
-int main(){
-    int cnt = 0,rowno = 0,colno = 0,arr[4][4] = {{1,2,3,11},{4,5,6,12},{7,8,9,13},{15,16,17,18}};
-    int lowlimrow = 0, upperlimrow = 5,lowlimcol = 0,upperlimcol = 5,rowcount = 0,colcount = 0,roworcol = 0,revornotcol =0,revornotrow = 0;
-    while(cnt <16){
-        if ((roworcol%2 )==0)
+int spriralprintcol(int arr[][4], int colno, int lowlim, int uperlim)
+{ // This function is going to print any row you wish
+    // This function will print alements of a specific col within a range this include lower limit but excludes upperlimit
+    int count = 0;
+    for (int i = lowlim; i < uperlim; i++)
+    {
+        printf("%d  ", arr[i][colno]);
+        count++;
+    }
+    return count;
+}
+
+int spriralprintcolrev(int arr[][4], int colno, int lowlim, int uperlim)
+{ // This function is going to print any row you wish
+    // This function will print alements of a specific col within a range this include lower limit but excludes upperlimit
+    int count = 0;
+    for (int i = (uperlim - 1); i >= lowlim; i--)
+    {
+        printf("%d  ", arr[i][colno]);
+        count++;
+    }
+    return count;
+}
+int main()
+{
+    int cnt = 0, rowno = 0, colno = 0, arr[4][4] = {{1, 2, 3, 11}, {4, 5, 6, 12}, {7, 8, 9, 13}, {15, 16, 17, 18}};
+    int lowlimrow = 0, upperlimrow = 5, lowlimcol = 0, upperlimcol = 5, rowcount = 0, colcount = 0, roworcol = 0, revornotcol = 0, revornotrow = 0;
+    while (cnt < 16)
+    {
+        if ((roworcol % 2) == 0)
         {
-            if ((revornotrow%2) == 0)
+            if ((revornotrow % 2) == 0)
             {
                 cnt += spriralprintrow();
-                upperlimrow --;
-                lowlimcol ++;
+                upperlimrow--;
+                lowlimcol++;
                 revornotrow++;
             }
-            else{
+            else
+            {
                 cnt += spriralprintrowrev();
-                lowlimrow ++;
-                upperlimcol --;
+                lowlimrow++;
+                upperlimcol--;
                 revornotrow++;
             }
-            
-           
         }
-        else{
-            if ((revornotcol%2) == 0)
+        else
+        {
+            if ((revornotcol % 2) == 0)
             {
                 cnt += spriralprintcol();
-                upperlimcol --;
-                
+                upperlimcol--;
+
                 revornotcol++;
             }
-            else{
+            else
+            {
                 cnt += spriralprintcolrev();
-                lowlimcol ++;
+                lowlimcol++;
                 revornotcol++;
             }
         }
