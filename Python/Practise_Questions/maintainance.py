f0c0d228067792b3f5f0969ee8ef684bbd2035c4
sksatyam <killersksatyam@gmail.com>
2023-12-27 23:27:12 +0530

    nothing new

--- Python/Practise_Questions/maintainance.py
+++ Python/Practise_Questions/maintainance.py
@@ -1,3 +1,4 @@
 print("this code is just written to maintain the strike of 1000 days" )
 print("This is maintainance modefication")
 print("sick leave")
+print("taking leave for now to search sreas")
