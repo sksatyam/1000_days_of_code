01a77e1b573148c564eda5c212973aa52e662190
sksatyam <killersksatyam@gmail.com>
2024-03-07 10:41:04 +0530

    implementing queue

---
+++ DSA/Data Structures/Queue/implementing.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+#define max 100
+class queue{
+    int arr[max];
+    int top =-1;
+    int front,rear;
+    void push(){
+        if (top ==max-1){
+            cout<<"Queue is Full!";
+        }
+        else{
+            
+            cout<<"\nEnter an Element: ";
+            cin>>arr[++top];
+        }
+    }
+    void pop(){}
+
+};
+
+int main(){
+    cout<<"hello world";
+    return 0;
+}
