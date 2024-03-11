0e134754300675ee57104e7e779f97a9c66c4716
sksatyam <killersksatyam@gmail.com>
2024-03-12 02:19:18 +0530

    Queue Implementing.cpp is not working!

--- DSA/Data Structures/Queue/implementing.cpp
+++ DSA/Data Structures/Queue/implementing.cpp
@@ -3,25 +3,50 @@ using namespace std;
 #define max 100
 class queue{
     int arr[max];
-    int top =-1;
+    int top =-1,bottom=0;
     int front,rear;
+    public:
+
     void push(){
         if (top ==max-1){
-            cout<<"Queue is Full!";
+            cout<<"Queue is Full!"<<endl;
         }
         else{
             
-            cout<<"\nEnter an Element: ";
+            cout<<"\nEnter an Element: "<<endl;
             cin>>arr[++top];
         }
     }
     void pop(){
-        
+        for(int i = 0;i<top;i++){
+            arr[i] = arr[i+1];
+        }
+        top --;
     }
 
 };
 
 int main(){
-    cout<<"hello world";
+    queue q1;
+    char user = 'n';
+    do{
+        int choice;
+        cout<< "Enter Your Choice: "<<endl;
+        cout<<"1. Push"<<endl<<"2. Pop"<<endl;
+        cin >> choice;
+        switch (choice){
+            case 1:
+                q1.push();
+            case 2:
+                q1.pop();
+            default:
+                cout <<"Enter a Valid Choice!"<< endl;
+                cin >> user;
+        }
+        cout<< "Do You want to coninue(y/n): ";
+
+
+    }while(user == 'y' || user == 'Y');
+
     return 0;
 }
