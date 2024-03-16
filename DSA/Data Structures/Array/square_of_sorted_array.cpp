9fc15cc753f8c3b786899f92dda4c1a563f98656
sksatyam <killersksatyam@gmail.com>
2024-03-17 03:36:30 +0530

    boiler plate of next problem

---
+++ DSA/Data Structures/Array/square_of_sorted_array.cpp
@@ -0,0 +1,14 @@
+// Given an integer array 'a' sorted in non-decreasing a order, return an array of the squares if each number sorted un non-decreasing order.
+
+#include<iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int size;
+    vector<int> vec;
+    cout << "Enter the Size of array: ";
+    cin>>size;
+    cout << endl<< "Enter the Elements: ";
+    for (int i = 0; i<)
+    return 0;
+}
