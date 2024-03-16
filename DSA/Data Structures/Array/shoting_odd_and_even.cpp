f27413f1f164d70188798728dfc2da5c01159113
sksatyam <killersksatyam@gmail.com>
2024-03-16 23:45:46 +0530

    First commit of the day

---
+++ DSA/Data Structures/Array/shoting_odd_and_even.cpp
@@ -0,0 +1,7 @@
+// Write a program to sort a array. put the odd evements at the begaining of the array and even numbers at the end of thearray
+#include<iostream>
+using namespace std;
+int main(){
+    
+    return 0;
+}
