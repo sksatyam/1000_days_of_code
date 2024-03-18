b29f8a304652d6850f8d3e7212f3b46fd80abbf6
sksatyam <killersksatyam@gmail.com>
2024-03-18 23:39:10 +0530

    boiler plate and basic outline for new problem

---
+++ DSA/Data Structures/Array/array_part.cpp
@@ -0,0 +1,16 @@
+// Check if we can partition the array into two subarray with equal sum. More formally, check that the prefix sum of a part of the array is equal to the suffix sum of rest of the array.
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int size;
+    vector<int> vec;
+    cout << "Enter the Size of the array: ";
+    cin>> size;
+    for (int i = 0; i <size;i++){
+        for(int j  = )
+    }
+
+    return 0;
+}
