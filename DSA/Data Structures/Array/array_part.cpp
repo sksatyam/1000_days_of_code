f4bb3c7cfef0fde266678e75513d2a78f547f1ba
sksatyam <killersksatyam@gmail.com>
2024-03-19 00:05:52 +0530

    array_part.cpp solved

--- DSA/Data Structures/Array/array_part.cpp
+++ DSA/Data Structures/Array/array_part.cpp
@@ -4,12 +4,36 @@
 #include<vector>
 using namespace std;
 int main(){
-    int size;
+    int size,i;
     vector<int> vec;
     cout << "Enter the Size of the array: ";
     cin>> size;
-    for (int i = 0; i <size;i++){
-        for(int j  = )
+    cout<<"Enter the Elements: ";
+    for(int i = 0;i<size;i++){
+        int tmp;
+        cin>>tmp;
+        vec.push_back(tmp);
+    }
+
+bool flag = false;
+    for (i = 0; i <size;i++){
+        int prefix_sum=0,suffix_sum=0;
+        for(int j  =0;j<=i;j++){
+            prefix_sum += vec[j];
+        }
+        for(int j = size-1;j>i;j--){
+            suffix_sum += vec[j];
+        }
+        if(prefix_sum == suffix_sum){
+            flag = true;
+            break;
+        }
+    }
+    if(flag == true){
+        cout <<i<<" ";
+    }
+    else{
+        cout<<"Not any possible";
     }
 
     return 0;
