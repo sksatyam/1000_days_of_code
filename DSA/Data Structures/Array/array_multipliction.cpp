252192379a1f1ee9b20f0abbb27f3371d822d3ba
sksatyam <killersksatyam@gmail.com>
2024-03-23 04:34:10 +0530

    array_multipliction.cpp solved

--- DSA/Data Structures/Array/array_multipliction.cpp
+++ DSA/Data Structures/Array/array_multipliction.cpp
@@ -1,61 +1,56 @@
-// Write a Program to multiply to Two arrays
 #include<iostream>
 using namespace std;
-int main(){
-    int row,col;
-    cout << "Enter the no. of Rows and Columns of the first array: ";
-    cin>> row>>col;
+
+int main() {
+    int row, col;
+    cout << "Enter the number of Rows and Columns of the first array: ";
+    cin >> row >> col;
+
     int arr[row][col];
-    cout<<"Enter the Elements for first Array: "<<endl;
-    for(int i = 0;i<row;i++){
-        for(int j = 0;j<col;j++){
-            cin>>arr[i][j];
+    cout << "Enter the Elements for the first Array: " << endl;
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < col; j++) {
+            cin >> arr[i][j];
         }
     }
 
-    int row2,col2;
-    cout << "Enter the no. of Rows and Columns of the Second array: ";
-    cin>> row2>>col2;
+    int row2, col2;
+    cout << "Enter the number of Rows and Columns of the Second array: ";
+    cin >> row2 >> col2;
+
+    if(col != row2) {
+        cout << "Multiplication Not Possible!" << endl;
+        return 0;
+    }
+
     int arr2[row2][col2];
-    cout<<"Enter the Elements for Second Array: "<< endl;
-    for(int i = 0;i<row2;i++){
-        for(int j = 0;j<col2;j++){
-            cin>>arr2[i][j];
+    cout << "Enter the Elements for Second Array: " << endl;
+    for(int i = 0; i < row2; i++) {
+        for(int j = 0; j < col2; j++) {
+            cin >> arr2[i][j];
         }
     }
 
-    // multiplication from now on
-1 2 3
-4 5 6
-
-1 2
-3 4
-5 6
-
-
-
-
-    int arr_ans[col][row2];
-    if(row ==col2){
-        // Logic Here
-        for(int i = 0; i<col;i++){
-            for (int j = 0;j<row2;j++){
-                int temp=0;
-                for (int k =0;k<col;k++){
-                    temp+=arr[j][col]*arr2[col][j];
-                }
-                arr_ans[i][j] = temp;
-                cout << arr_ans[i][j]<<" ";
+    int arr_ans[row][col2]; // Resultant matrix dimensions: (row x col2)
+    // Matrix Multiplication Logic
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < col2; j++) {
+            int temp = 0;
+            for(int k = 0; k < col; k++) {
+                temp += arr[i][k] * arr2[k][j];
             }
-            cout << endl;
+            arr_ans[i][j] = temp;
         }
-
-
-    }
-    else{
-        cout << "Multiplication Not Possible!";
     }
 
+    // Output the Resultant Matrix
+    cout << "Resultant Matrix after Multiplication:" << endl;
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < col2; j++) {
+            cout << arr_ans[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
