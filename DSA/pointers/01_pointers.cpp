6ae662eded463b2bfed879dfaee006f3ea136b7e
sksatyam <killersksatyam@gmail.com>
2024-04-01 17:13:21 +0530

    basics of pointer

--- DSA/pointers/01_pointers.cpp
+++ DSA/pointers/01_pointers.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
+    // creating a pointer: datatype *pointername(datatype of the pointer must be the same as the datatype of the variable of which it is going to store address)
+    int x = 12;
+    int *p = &x; // '&' is called address of operator is used to access the address of any variable;
+
+    
+    
     int arr[3] = {4,6,10};
     int *ptr = &arr[1];
+
+
 
     // cout << (*ptr)++ << " " <<*ptr; // In this case where we first derefferencing and using postincrement then in this case the pointer will not affected but the value that stored at the point will be affected
     // cout << *ptr++ << " " <<*ptr; // In this case the pointer is incrementing instead of value because it reads from right to left
