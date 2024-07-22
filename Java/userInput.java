64ec3f7f408cc807efd04dc7727477a68f5864c9
sksatyam <killersksatyam@gmail.com>
2024-07-23 02:41:09 +0530

    closed scan object

--- Java/userInput.java
+++ Java/userInput.java
@@ -6,5 +6,6 @@ public class userInput {
         System.out.println("enter a number:");
         int num = scan.nextInt();
         System.out.println("the Number is: "+num);
+        scan.close(); // this will close the object scan so that our program can be memory efficient
     }
 }
