ddfa5922f0ae3546f72d7254186b578196cd160f
sksatyam <killersksatyam@gmail.com>
2024-07-26 02:02:23 +0530

    ternury operator basics added

--- Java/conditionals.java
+++ Java/conditionals.java
@@ -1,7 +1,14 @@
 public class conditionals {
     public static void main(String[] args){
 
-        // all the syntax for the conditionals statements are asme as c++ like if else, elseif and ternary operators and also in switch case statements
-
+        // all the syntax for the conditionals statements are asme as c++ like if else, elseif and ternary operators and also in switch case statements        
+        // syntax for ternury operator
+        // condition ? expression1: Expression2 :- if contion is true then expression 1 will evaluate other wise expression2 will evailuate and even we can create nested ternary operators
+        // example 
+        int a = 2;
+        int b = 4;
+        int max  = 0;
+        max = a>b ? a:b;
+        System.out.println(max);
     }
 }
