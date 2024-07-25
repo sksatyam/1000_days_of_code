dd6e338d5b812f59179055064f4e79ef6f370962
sksatyam <killersksatyam@gmail.com>
2024-07-26 01:55:54 +0530

    theory on conditional statements

--- Java/conditionals.java
+++ Java/conditionals.java
@@ -1,3 +1,7 @@
 public class conditionals {
-    
+    public static void main(String[] args){
+
+        // all the syntax for the conditionals statements are asme as c++ like if else, elseif and ternary operators and also in switch case statements
+
+    }
 }
