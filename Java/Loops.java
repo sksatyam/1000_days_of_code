4dd73da04d66a3fd213c2e8e4ea34e5427392af8
sksatyam <killersksatyam@gmail.com>
2024-07-26 02:42:56 +0530

    example code for Labeled break and continue is added

--- Java/Loops.java
+++ Java/Loops.java
@@ -2,6 +2,31 @@ public class Loops {
     public static void main(String[] args){
         // All the loops of java is same as C++, there is no difference in syntax and consept of loops between java and c++ 
         // Syntax will be added later on
+        // Labled breaek and labled continue is same as goto statement in c++
+        // Labeled break
+
+                outerLoop:
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        if (j == 1) {
+                            break outerLoop;
+                        }
+                        System.out.println("i: " + i + ", j: " + j);
+                    }
+                }
+
+        // Labeled Continue
+
+                outerLoop:
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                     if (j == 1) {
+                         continue outerLoop;
+                     }
+                        System.out.println("i: " + i + ", j: " + j);
+                    }
+                }
     }
-    
 }
+        
+
