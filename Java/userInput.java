5c24d2ed4e6510d1c1f6e357bb2f529df8960ca4
sksatyam <killersksatyam@gmail.com>
2024-07-25 23:32:30 +0530

    comment modefication

--- Java/userInput.java
+++ Java/userInput.java
@@ -6,6 +6,6 @@ public class userInput {
         System.out.println("enter a number:");
         int num = scan.nextInt();
         System.out.println("the Number is: "+num);
-        scan.close(); // this will close the object scan so that our program can be memory efficient
+        scan.close(); // this will close the object scan so that our program can be memory efficient, It is not necessary because garbage collactor in java does this automatically
     }
 }
