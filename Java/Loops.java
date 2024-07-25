94be0e183d58a26a6713fce71d507563dc7a5863
sksatyam <killersksatyam@gmail.com>
2024-07-26 02:30:40 +0530

    theory for loops in java compared to c++

---
+++ Java/Loops.java
@@ -0,0 +1,7 @@
+public class Loops {
+    public static void main(String[] args){
+        // All the loops of java is same as C++, there is no difference in syntax and consept of loops between java and c++ 
+        // Syntax will be added later on
+    }
+    
+}
