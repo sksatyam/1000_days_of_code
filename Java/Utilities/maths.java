283b99cdec1ac529d48373fcbe78ef8fcc81439c
sksatyam <killersksatyam@gmail.com>
2024-07-27 00:46:29 +0530

    practising with packages; added Utilities package

---
+++ Java/Utilities/maths.java
@@ -0,0 +1,17 @@
+package Utilities;
+
+public class maths {
+    public static int add(int a,int b){
+        return a+b;
+        
+    }
+    public static int subtract(int a, int b){
+        return a-b;
+    }
+    public static int multiplication(int a, int b){
+        return a*b;
+    }
+    public static int division(int a,int b){
+        return a/b;
+    }
+}
---
+++ Java/Utilities/strings.java
@@ -0,0 +1,15 @@
+package Utilities;
+
+public class strings {
+    public static void concatinate(String a,String b){
+        System.out.println(a+" "+b);
+    }    
+    // public static void first(String a){
+        
+    // }
+    // public static void traverse(String a){
+    //     for (int i = 0; i < a.length(); i++) {
+    //         System.out.println(a[i]);
+    //     }
+    // }
+}
