952462c73bce80cc6c47b2aed2618457699202cc
sksatyam <killersksatyam@gmail.com>
2024-07-27 00:48:17 +0530

    mainprogram.java added under mainPackage

---
+++ Java/mainPackage/mainprogram.java
@@ -0,0 +1,14 @@
+package mainPackage;
+
+import Utilities.maths;
+import Utilities.strings;
+
+public class mainprogram {
+    public static void main(String[] args) {
+        System.out.println("Addition is:- "+maths.add(12, 12));
+        System.out.println("Subtraction is:- "+ maths.subtract(12, 6));
+        strings.concatinate("Sanjib", "is a good boy");
+    }
+
+
+}
