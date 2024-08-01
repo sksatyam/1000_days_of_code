b61695ab8c48bd7ed69eabdba3497e9090979517
sksatyam <killersksatyam@gmail.com>
2024-08-01 23:16:28 +0530

    for sync

---
+++ Java/OOPS/final_keyword.java
@@ -0,0 +1,8 @@
+package OOPS;
+
+public class final_keyword {
+    // final keyword can be used with variables, methods and classes
+    // if we creates a variable with final keyword then a immutable variable is created
+    // 
+    
+}
