15ee5d6c72ad05b24d0dac82b52ad17ccdee38d1
sksatyam <killersksatyam@gmail.com>
2024-08-01 17:45:09 +0530

    comparision of strings

--- Java/strings.java
+++ Java/strings.java
@@ -1,7 +1,7 @@
 public class strings {
     public static void main(String[] args) {
         // There are two ways of creating an string
-        String s1 = new String("Sanjib"); // in this way the literals are created into heap memory directly as objects 
+        String s1 = new String("Sanjib"); // in this way the literals are created into heap memory directly as objects, every time it will create a new object even if for the same string
         String s2 = "Sanjib"; // But in this way the literals are created into "string constant pool" which is reserved area in the heapp memory, this area is created to optimize the strings in java. when we creates a literal using this method if the literals is already exists in the string pool then it just create a new refference to that literal instead of creating of new literal into the memory to save memory.
         
         // Difference in == operator and .equals() method
@@ -24,6 +24,11 @@ public class strings {
         */
         
         // Checking the equility of strings
+        String s3 = new String("Sanjib");
+        boolean bool = s1 == s3 ? true:false; // even if the both strings are same but the value stored in the bool is false because == operators coompares refferences only and s1 and s3 are at different memory location as they both are created using new keywords so two different objects are created for them in heap memory. So to compare values we uses equals() methhod
+        System.out.println(bool);
+
+        // In Java strings are immutable because when we changes the value then it starts reffering to new object not changes and later on java's garbage collector removes it from memory.
 
     }
 }
