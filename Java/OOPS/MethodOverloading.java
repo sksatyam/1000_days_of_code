1e6fc212019caea60ed07abd3b0f07e56945bf9c
sksatyam <killersksatyam@gmail.com>
2024-08-01 20:33:38 +0530

    Methodoverloading Theory

---
+++ Java/OOPS/MethodOverloading.java
@@ -0,0 +1,12 @@
+package OOPS;
+
+public class MethodOverloading {
+    // two or more methods can have same name inside a same class if they accepts different arguments. this featre is knows as method overloading.
+
+    // Method overloading is achieved by either:
+    //    1) Changing the number of arguments
+    //     2) or changing te datatype of the arguments 
+    
+    // It is not methodoverloading if we only change the return type of methods.
+    //    There must be differences in the nmber of parameters.
+}
