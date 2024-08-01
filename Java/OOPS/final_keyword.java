66ec0b5fb0397ab6235bb92f35a5bac70bd38fc6
sksatyam <killersksatyam@gmail.com>
2024-08-01 23:19:19 +0530

    final keyword theory

--- Java/OOPS/final_keyword.java
+++ Java/OOPS/final_keyword.java
@@ -3,6 +3,7 @@ package OOPS;
 public class final_keyword {
     // final keyword can be used with variables, methods and classes
     // if we creates a variable with final keyword then a immutable variable is created
-    // 
+    // if we creates a method using final keyword then this method doesnt allows to be overridding in child classes
+    // if we create a class with final keyword then it doesntt allows inheritance.
     
 }
