b1be6bd41215338ee5ddf563911eb54800d03279
sksatyam <killersksatyam@gmail.com>
2024-08-14 20:04:38 +0530

    add a note on calling a perameterised constructor of super class in file Constructor3.java

---
+++ Java/Quick_Reminders/Constructor3.java
@@ -0,0 +1,5 @@
+package Quick_Reminders;
+
+public class Constructor3{
+    // If we want to call a Parameterised Constructor of a paresnt class from the child class and alse we want to pass the value which we will take while creating the object then er have to create a constructor of the child class to take data while greating the object and then we will use super keyword with perameter from inside the constructor
+}
