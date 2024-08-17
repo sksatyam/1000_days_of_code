4219e2dba2471e6d43eef12797e4ad756cf7662b
sksatyam <killersksatyam@gmail.com>
2024-08-17 23:40:55 +0530

    ananomus object consepts added

---
+++ Java/OOPS/Consepts/AnanomusObjects.java
@@ -0,0 +1,15 @@
+package OOPS.Consepts;
+
+class Demo{
+    public void Printer(){
+        System.out.println("Inside the Demo class");
+    }
+}
+
+public class AnanomusObjects {
+    public static void main(String[] args) {
+        // Demo newObj = new Demo(); // here what we are doing is we are creating a object and storing it into a reference type variable
+
+        new Demo().Printer(); // this is how we creates an ananomus object what we deo here we just creates an object and use it and we are not storing it into any refference variable so it is not reusable
+    }
+}
