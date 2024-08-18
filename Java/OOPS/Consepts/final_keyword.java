a05fa9919ad3b70822e335a20d82a45c6aec3fc3
sksatyam <killersksatyam@gmail.com>
2024-08-19 00:52:47 +0530

    Added Examples for syntax

--- Java/OOPS/Consepts/final_keyword.java
+++ Java/OOPS/Consepts/final_keyword.java
@@ -3,7 +3,15 @@ package OOPS.Consepts;
 public class final_keyword {
     // final keyword can be used with variables, methods and classes
     // if we creates a variable with final keyword then a immutable variable is created
+    // Example
+    final int num1 = 12;
     // if we creates a method using final keyword then this method doesnt allows to be overridding in child classes
+    public final void show(){
+        System.out.println("Nothing!");
+    }
     // if we create a class with final keyword then it doesntt allows inheritance.
+    final class Hello{
+        
+    }
     
 }
