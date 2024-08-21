ea41c2639ca4329966fcc10a401afb70ca7a3907
sksatyam <killersksatyam@gmail.com>
2024-08-21 23:56:23 +0530

    Basic Innerclass Structure formed

--- Java/OOPS/Consepts/InnerClass.java
+++ Java/OOPS/Consepts/InnerClass.java
@@ -1,5 +1,24 @@
 package OOPS.Consepts;
 
+
+/**
+ * Basic Inner class Structure
+*/
+class A{
+    public void printHelloWorld(){
+        System.out.println("Hello World From A!");
+    }
+
+    class InnerInnerClass {
+        public void configPrinter(){
+            System.out.println("No config! LOL");
+        }
+        
+    }
+}
 public class InnerClass {
+    public static void main(String[] args) {
+        
+    }
     
 }
