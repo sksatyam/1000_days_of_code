5b8a55eb4b69712689828773c74c5f41bec20dd0
sksatyam <killersksatyam@gmail.com>
2024-08-22 01:10:38 +0530

    Anonymous Inner class

---
+++ Java/OOPS/Consepts/AnonymousInnerClass.java
@@ -0,0 +1,23 @@
+package OOPS.Consepts;
+
+class A{
+    public void sanjibPrinter(){
+
+    }
+
+}
+
+public class AnonymousInnerClass {
+    public static void main(String[] args) {
+        A obj = new A(){ // this is called Anonymous Inner class and this even works for Abstract type outer class(Means we can even create the ananymous inner class of abstract class as its create the obect the inner class ) 
+            public void sanjibPrinter(){
+                System.out.println("Sanjib!");
+            }
+        };
+
+        obj.sanjibPrinter();
+
+    }
+
+    
+}
