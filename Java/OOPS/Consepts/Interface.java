5ffbb5dce71bfbd174c72616d1f88f31433adb7e
sksatyam <killersksatyam@gmail.com>
2024-08-24 22:57:39 +0530

    for Sync

--- Java/OOPS/Consepts/Interface.java
+++ Java/OOPS/Consepts/Interface.java
@@ -1,11 +1,14 @@
 package OOPS.Consepts;
 
 // In interface by default all the functions are public and abstract
-interface A{
+interface A {
     void sayHello();
 
 }
 
 public class Interface {
-    
+    public static void main(String[] args) {
+        System.out.println("All ok");
+    }
+
 }
