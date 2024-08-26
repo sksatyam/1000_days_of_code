eada2824850ebd9443ba9352f196993001f1597b
sksatyam <killersksatyam@gmail.com>
2024-08-27 00:58:06 +0530

    Triangle.java: solved

--- Java/PatternProblems/Triangle.java
+++ Java/PatternProblems/Triangle.java
@@ -1,5 +1,34 @@
+/*
+Write a program to print the pattern
+*
+**
+***
+****
+*****
+*/
+// Importing required Packages
+import java.util.Scanner;
+
+
+// Created public class 
 public class Triangle {
+	
+	// main method
 	public static void main(String[] args) {
-		System.out.println("All set ready to code");
+
+		// creating object of Scanner class to take input from user
+		Scanner sc  = new Scanner(System.in);
+
+		System.out.print("Enter the number of Rows: ");
+		int userInp = sc.nextInt();
+		// System.out.println("\n");
+
+		for (int i = 0; i < userInp; i++) {
+			for (int j = 0 ;j <= i;j++){
+				System.out.print("* ");
+			}
+			System.out.print("\n");
+		}
+
 	}
 }
