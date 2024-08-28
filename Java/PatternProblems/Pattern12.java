50687a870da9a2fe6bd46a8044cf37980dae8e6b
sksatyam <killersksatyam@gmail.com>
2024-08-28 11:37:51 +0530

    Pattern12.java added but not solved yet

---
+++ Java/PatternProblems/Pattern12.java
@@ -0,0 +1,21 @@
+
+// 1      1
+// 12    21
+// 123  321
+// 12344321
+
+import java.util.Scanner;
+public class Pattern12{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int userInp = sc.nextInt();
+        sc.close();
+
+        for(int i = 0;i<userInp;i++){
+            for(int j = 0;j<(userInp*2);j++){
+                if()
+            }
+        }
+    }
+}
