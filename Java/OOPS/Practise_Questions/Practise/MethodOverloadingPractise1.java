b6f12e39fadfdf3c2ba36c0b53e9195c86a4ae6b
sksatyam <killersksatyam@gmail.com>
2024-08-28 23:45:04 +0530

    removed pacage name

--- Java/OOPS/Practise_Questions/Practise/MethodOverloadingPractise1.java
+++ Java/OOPS/Practise_Questions/Practise/MethodOverloadingPractise1.java
@@ -1,16 +1,19 @@
-package OOPS.Practise_Questions.Practise;
-class calc{
-    public void add(int a,int b){
-        System.out.println("Sum is : "+ (a+b));
+// package OOPS.Practise_Questions.Practise;
+class calc {
+    public void add(int a, int b) {
+        System.out.println("Sum is : " + (a + b));
     }
-    public void add(int a, int b,int c){
-        System.out.println("Sum is : "+ (a+b+c));
+
+    public void add(int a, int b, int c) {
+        System.out.println("Sum is : " + (a + b + c));
     }
-    public void add(double a,double b){
-        System.out.println("Sum is : "+(a+b));
+
+    public void add(double a, double b) {
+        System.out.println("Sum is : " + (a + b));
     }
-    public void add(String a,String b){
-        System.out.println("Sum is : "+(a+b));
+
+    public void add(String a, String b) {
+        System.out.println("Sum is : " + (a + b));
     }
 }
 
@@ -20,12 +23,12 @@ public class MethodOverloadingPractise1 {
         System.out.println("Two Int add method! ");
         nc.add(12, 13);
         System.out.println("Three Int add Mehtod! ");
-        nc.add(2, 2,2);
+        nc.add(2, 2, 2);
         System.out.println("Two Double add Method! ");
         nc.add(12.2d, 12.3d);
         System.out.println("Two String add Method! ");
         nc.add("1", "1");
 
     }
-    
+
 }
