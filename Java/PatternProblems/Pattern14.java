13763ed63796d781a6fd4e3b10debd6877c7cbb4
sksatyam <killersksatyam@gmail.com>
2024-09-01 23:34:16 +0530

    Pattern14.java added

--- Java/PatternProblems/Pattern14.java
+++ Java/PatternProblems/Pattern14.java
@@ -1,1 +1,24 @@
+//    A
+//    AB
+//    ABC
+//    ABCD
+//    ABCDE
 
+import java.util.Scanner;
+
+public class Pattern14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of Rows: ");
+        int userInp = sc.nextInt();
+        sc.close();
+        int ascii = 65;
+        for (int i = 0; i < userInp; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print((char) (ascii + j));
+            }
+            System.out.println();
+
+        }
+    }
+}
