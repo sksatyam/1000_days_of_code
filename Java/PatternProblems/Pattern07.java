0c39890b0dade7cd0b7d20d144b3d35cb1696724
sksatyam <killersksatyam@gmail.com>
2024-09-03 18:10:28 +0530

    Pattern07.java added

--- Java/PatternProblems/Pattern07.java
+++ Java/PatternProblems/Pattern07.java
@@ -7,33 +7,32 @@ wtite a program to print the given pattern
   *******
  *********
 ***********
-*/ 
+*/
 
 import java.util.Scanner;
 
 public class Pattern07 {
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of Rown: ");
         int userInp = sc.nextInt();
         sc.close();
 
         for (int i = 0; i < userInp; i++) {
-            for (int j = 0; j < userInp+(userInp-1); j++) {
-                if (j < userInp-i-1){
-                    System.out.print("  ");
-                }
-                int x = userInp-i-1;
-                if (j>=x) {
-                    System.out.print(" *");
-                }
+            for (int j = (userInp - i - 1); j > 0; j--) {
+                System.out.print("  ");
 
-                
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
             }
             System.out.println();
             // System.out.println();
         }
 
-    }    
+    }
 }
