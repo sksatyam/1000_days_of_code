392c750e468866c7be223e3437d28b823b0e9cde
sksatyam <69345899+sksatyam@users.noreply.github.com>
2024-09-10 16:45:39 +0530

    Update FinalKeyword.java

--- Java/OOPS/Concepts/FinalKeyword.java
+++ Java/OOPS/Concepts/FinalKeyword.java
@@ -1,9 +1,9 @@
 package OOPS.Consepts;
 
 public class final_keyword {
-    // final keyword can be used with variables, methods and classes
-    // if we creates a variable with final keyword then a immutable variable is created
-    // Example
+    // final keyword can be used with variables, methods and classes.
+    // if we creates a variable with final keyword then a immutable variable is created.
+    // Example:
     final int num1 = 12;
     // if we creates a method using final keyword then this method doesnt allows to be overridding in child classes
     public final void show(){
