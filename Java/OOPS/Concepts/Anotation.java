40f7207015e18a02896b42304d904e858f02313f
sksatyam <killersksatyam@gmail.com>
2024-09-18 00:33:05 +0530

    Anotation.java added

---
+++ Java/OOPS/Concepts/Anotation.java
@@ -0,0 +1,13 @@
+public class Anotation {
+    public static void main(String[] args) {
+        // In Java, an annotation is a form of metadata that provides data about the
+        // program but is not part of the program itself. Annotations are used to give
+        // instructions to the compiler, provide information for code analysis tools, or
+        // affect runtime behavior without directly changing the logic of the program.
+
+        // Annotations can be applied to classes, methods, variables, parameters, and
+        // other elements to influence how they are processed by tools and libraries.
+
+        // @Override: Indicates that a method is overriding a method from a superclass.
+    }
+}
