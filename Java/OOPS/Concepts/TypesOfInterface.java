88e60bd8c1b989039e37b246be6637a83eb6800c
sksatyam <killersksatyam@gmail.com>
2024-09-19 03:36:27 +0530

    TypesOfInterface.java added

---
+++ Java/OOPS/Concepts/TypesOfInterface.java
@@ -0,0 +1,9 @@
+public class TypesOfInterface {
+    public static void main(String[] args) {
+        // there are three types of Interfaces in java
+        // Normal or Regular Interfaces: It can cointain any number of abstract methods.
+        // Marker Interfaces: This is a interface with no methods or blank Interface
+        // Functional Interface: This Interface has only one method
+        // System.out.println("All Right!");
+    }
+}
